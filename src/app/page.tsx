cf93599a827d3fae450d8d467004b3f109de0cd1
Hien Giang <quachtrieuvu@gmail.com>
2024-06-25 22:38:32 +0700

    add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,18 @@
-import { Button } from "@nextui-org/react";
-import * as actions from "@/actions";
-import { auth } from "@/auth";
-import Profile from "@/components/profile";
+import TopicCreateForm from "@/components/topics/topic-create-form";
+import TopicList from "@/components/topics/topic-list";
+import { Divider } from "@nextui-org/react";
 export default async function Home() {
-  const session = await auth();
   return (
-    <div>
-      <form action={actions.signIn}>
-        <Button type="submit">Sign In</Button>
-      </form>
-      <form action={actions.signOut}>
-        <Button type="submit">Sign Out</Button>
-      </form>
-      {session?.user ? (
-        <div>{JSON.stringify(session.user)}</div>
-      ) : (
-        <div>Signed Out</div>
-      )}
-      <Profile />
-    </div>
+    <main className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-xl m-2">Top Posts</h1>
+      </div>
+      <div className="border shadow py-3 px-2">
+        <TopicCreateForm />
+        <Divider className="my-2" />
+        <h3 className="text-lg">Topics</h3>
+        <TopicList />
+      </div>
+    </main>
   );
 }
